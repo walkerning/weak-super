1a7078093849a1016a6ea0e4f453f88a8d27d9b2
walkerning <foxdoraame@gmail.com>
2016-06-04 12:00:18 +0800

    Modify interfaces of Dataset.

--- wksuper/dataset/__init__.py
+++ wksuper/dataset/__init__.py
@@ -20,11 +20,64 @@ class Dataset(object):
         for name, value in kwargs.iteritems():
             self.cfg[name] = value
 
-    def get_one_train_image(self):
+    @property
+    def class_number(self):
+        """
+        返回一个整数 `K`, 代表该数据集一共分多少类
+        """
         raise NotImplementedError()
+
+    def all_train_indexes(self):
+        """
+        返回所有训练(train)图像的索引列表 + 一一对应的标注列表
+        Returns
+        ------------
+        inds: numpy.array
+              1 x N. 每个元素为一张图像的索引. 
+        array_of_labels: numpy.array
+              N x K. 每行为一张图像的K类的0\1标注
+        """
+        raise NotImplementedError()
+
+    def all_val_indexes(self):
+        """
+        返回所有验证(val)图像的索引列表 + 一一对应的标注列表
+        Returns
+        ------------
+        inds: numpy.array
+              1 x N. 每个元素为一张图像的索引. 
+        array_of_labels: numpy.array
+              N x K. 每行为一张图像的K类的0\1标注
+        """
+        raise NotImplementedError()
+
+    def positive_train_indexes(self, _cls):
+        """
+        获得某种类型`_cls`的所有正例图像的索引列表
         
-    def get_one_test_image(self):
+        Parameters
+        ------------
+        _type: 类型编号
+
+        Returns
+        ------------
+        正例图像索引列表
+        """
         raise NotImplementedError()
+
+    def get_image_at_index(self, index):
+        """
+        获得某个索引对应的图像数据和对应的弱标注
+
+        Returns
+        ------------
+        im: numpy.array 
+            图像数据
+        labels: numpy.array
+            1 x K, 每个元素为1或0, 第k个元素代表此图片中有没有第k类物体
+        """
+        raise NotImplementedError()
+
 
 class VOCHandler(Dataset):
     """
@@ -40,9 +93,3 @@ class VOCHandler(Dataset):
     def __init__(self, cfg, **kwargs):
         super(VOCHandler, self).__init__(cfg, **kwargs)
         print "Use VOC dataset path: ", self.cfg["path"]
-        
-    def get_one_train_image(self):
-        pass
-        
-    def get_one_test_image(self):
-        pass
